a97f89e1e47943b3921a0e5bf724859db8506e04
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-02-07 14:16:46 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, StudentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
